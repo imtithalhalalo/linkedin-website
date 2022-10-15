9895ab7eb0772c538e461d026073df9192216e78
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-16 01:57:44 +0300

    frontend: add needed routes and page path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Login from './components/Login';
 import SignUp from "./components/SignUp";
-
+import UserProfile from "./components/UserPage/UserProfile";
+import CreatePost from "./components/CompanyPage/CreatePost";
+import NavUser from "./components/NavUser";
+import Jobs from "./components/UserPage/Jobs"
 function App() {
-  const [page,setPage] = useState('');
+  const [path, setPath] = useState('./login');
+
+  const getPath = (role) => {
+		switch (role) {
+			case 'user':
+				setPath('/user');
+				break;
+			case 'company':
+				setPath('/company');
+				break;
+			default:
+				setPath('/login');
+				break;
+		}
+	}
   return (
     <BrowserRouter>
       <Routes>
@@ -12,8 +29,11 @@ function App() {
           <SignUp />
         }/>
         <Route path='/login' element={
-          <Login />
+          <Login onLogin={getPath}/>
         }/>
+        <Route path='/user' element={<><NavUser/> <UserProfile /></>} />
+				<Route path='/company' element={<CreatePost />} />
+        <Route path='/jobs' element={<><NavUser/><Jobs /></>} />
       </Routes>
     </BrowserRouter>
   );
