cc242770d49ca4195286ef81f1421366c5b9290a
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-16 01:46:24 +0300

    backend: modify users model

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -16,7 +16,20 @@ const userSchema = new mongoose.Schema({
         required: 'password is required',
         select: false
     },
-
+    location: {
+        type: String
+    },
+    education: {
+        type: String
+    },
+    experience: {
+        type: String
+    },
+    image: {
+        type: String,
+        default: '../public/images/default.png'
+    },
+    role: String
 })
 
 const User = mongoose.model('User', userSchema);
