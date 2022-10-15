f411f0237a052150f8e9d117b0ecc60885614510
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-16 01:51:06 +0300

    backend: add get jobs api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+const Job = require('../models/jobs.model');
 const User = require('../models/users.model');
 
 const editProfile = async (req, res)=>{
@@ -16,7 +17,17 @@ const getUser = async (req, res) => {
     res.status(200).json({"user":user});
 }
 
+const getJobs = async (req, res) => {
+    const job = Job.find().then(
+        (job) => {
+            res.status(200).json({"message" : "All Jobs", "job":job})
+        }
+    )
+    .catch((err)=>res.status(400).send(err)) 
+
+}
 module.exports = {
     editProfile,
-    getUser
+    getUser,
+    getJobs
 }
