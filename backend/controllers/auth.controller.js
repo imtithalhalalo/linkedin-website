13c05c5f81ee499d86263e2de7111dcdd4a71485
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-15 11:47:23 +0300

    backend: add login funtion api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,24 @@
 const User = require('../models/users.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const login = async (req, res)=>{
+    const {email, password} = req.body;
+    
+    const user = await User.findOne({email}).select("+password");
+    
+    if(!user) return res.status(404).json({message: "Invalid Credentials"});
+
+    const isMatch = bcrypt.compare(password, user.password);
+    if(!isMatch) return res.status(404).json({message: "Invalid Credentials"});
+
+    const token = jwt.sign(
+        {email: user.email, name: user.name}, 
+        process.env.JWT_SECRET_KEY, 
+        {expiresIn: '1h'}
+        );
+    res.status(200).json({'token':token})
+}
 
 const signup = async (req, res)=>{
     const {name, email, password} = req.body;
@@ -18,5 +38,6 @@ const signup = async (req, res)=>{
 }
 
 module.exports = {
+    login,
     signup
 }
