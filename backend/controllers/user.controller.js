7c4a7a1756b1d956bbf7ee3bec4edb189a591c03
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-16 01:50:35 +0300

    backend: add get user api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -10,6 +10,13 @@ const editProfile = async (req, res)=>{
     .catch((err)=>res.status(400).send(err)) 
 }
 
+const getUser = async (req, res) => {
+    const {id} = req.params;
+    const user = await User.findById(id);
+    res.status(200).json({"user":user});
+}
+
 module.exports = {
     editProfile,
+    getUser
 }
