708aa190a630908f59e96ff43d34d8a1e599439d
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-16 22:34:51 +0300

    frontend: add apply hook

---
+++ frontend/src/hooks/apply.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+
+export const apply = async (applicant) => {
+    const data = {
+        user: applicant.user,
+        job: applicant.job,
+        email: applicant.email,
+        phonenum: applicant.phonenum
+    }
+    console.log(applicant.user, applicant.job)
+    await axios.post(`http://localhost:3001/user/apply`, data, { headers: { 'Authorization': `Bearer ${localStorage.getItem(`token`)}` } }).then(response => {
+     return response.data.message;
+    }).catch((err) => { alert(err) });  
+};
