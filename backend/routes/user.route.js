c92ef7139875ef4e22a3743f84e2250bfe953841
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-16 22:30:55 +0300

    backend: add user route apply

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
-const {editProfile, getUser, getJobs, search} = require('../controllers/user.controller')
+const {editProfile, getUser, getJobs, search, apply} = require('../controllers/user.controller')
 const router = Router();
 
 router.put('/', editProfile);
 router.get('/getuser/:id', getUser);
 router.get('/jobs', getJobs);
 router.get('/search/:key', search);
+router.post('/apply', apply);
 module.exports = router;
