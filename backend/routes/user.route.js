992e8215085b91f34555ae03c7fb61e57a70c058
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-16 10:38:36 +0300

    backend: add search route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,9 @@
 const {Router} = require('express');
-const {editProfile, getUser, getJobs} = require('../controllers/user.controller')
+const {editProfile, getUser, getJobs, search} = require('../controllers/user.controller')
 const router = Router();
 
 router.put('/', editProfile);
 router.get('/getuser/:id', getUser);
 router.get('/jobs', getJobs);
+router.get('/search/:key', search);
 module.exports = router;
