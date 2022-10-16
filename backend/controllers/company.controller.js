1b57d50a970000d9ce60be2543f5a62ae6b1e7ec
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-16 22:29:20 +0300

    backend: add get profiles api

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/users.model');
 const Job = require('../models/jobs.model');
+const Applicant = require('../models/applicants.model');
 const createJobPost = async (req, res)=>{
     Job.create(req.body)
     .then((job)=> {
@@ -8,8 +9,16 @@ const createJobPost = async (req, res)=>{
     .catch((err)=>res.status(400).send(err)) 
 }
 
-
+const getProfiles = async (req, res) => {
+    const {job_id} = req.body;
+    Applicant.find({job: job_id}).populate("user")
+    .then((applicant)=> {
+        res.status(200).json({"message" : "All Applicants", "applicant":applicant})
+    })
+    .catch((err)=>res.status(400).send(err)) 
+}
 
 module.exports = {
-    createJobPost
+    createJobPost,
+    getProfiles
 }
