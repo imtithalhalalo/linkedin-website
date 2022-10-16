a39c2bdad4be5879a190b8938d7e6daee5d07bd0
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-16 22:33:47 +0300

    frontend: modify button to component

--- frontend/src/components/UserPage/Jobs.jsx
+++ frontend/src/components/UserPage/Jobs.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
+import ApplyModal from '../ApplyModal';
+import Button from '../Button';
 
 const Jobs = () => {
     const [jobs, setJobs] = useState([]);
     const id = localStorage.getItem('id');
+    const [jobId, setJobID] = useState('');
+    const [showModal, setShow] = useState(false);
     const getJobs = async () => {
         try {
             await axios.get(`http://localhost:3001/user/jobs`,
@@ -27,18 +31,19 @@ const Jobs = () => {
                     <div className="h-24 bg-blue w-2/4">
                         <h1 className="text-white text-4xl text-center pt-5"> Jobs </h1>
                     </div>
-                    {jobs.map(job => (
-                        <div className="bg-white h-48 w-2/4 border p-5">
+                    {jobs.map((job, index) => (
+                        <div className="bg-white h-48 w-2/4 border p-5" key={index}>
                             <div className="flex flex-col">
                                 <a className="text-lg text-blue-600">{job.title}</a>
                                 <h6 className="text-md text-black">{job.company_name}</h6>
                                 <h6 className="text-sm text-gray-500">{job.desc}</h6>
                             </div>
-                            <button className="text-center rounded-full w-48 bg-blue text-white py-3 font-medium mt-5">Easy Apply </button>
+                            <Button onClick={() => {setShow(true); setJobID(job._id); console.log(job._id)}} text={"Easy Apply"}/>
                         </div>
+                        
                     ))}
 
-
+                    <ApplyModal onClose={() => setShow(false)} show={showModal}  user_id={id} job_id={jobId}/>
                 </div>
             </div>
 
