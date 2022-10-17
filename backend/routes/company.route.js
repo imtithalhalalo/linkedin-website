e70f682f9c8770985a5b05752fbba79f09625265
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-17 13:09:32 +0300

    backend: add get company route

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -1,8 +1,10 @@
 const {Router} = require('express');
-const { createJobPost, getProfiles, getJobs } = require('../controllers/company.controller')
+const { createJobPost, getProfiles, getJobs, getCompanies } = require('../controllers/company.controller');
 const router = Router();
 
 router.post('/', createJobPost);
 router.post('/applicants', getProfiles);
-router.post('/jobs', getJobs)
+router.post('/jobs', getJobs);
+router.get('/companies', getCompanies);
+
 module.exports = router;
