46934ad8c887a2ecd47db466bd89e4ae9cb64378
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-17 22:56:14 +0300

    frontend: modify navbar

--- frontend/src/components/NavUser.jsx
+++ frontend/src/components/NavUser.jsx
@@ -4,9 +4,10 @@ import { FaShoppingBag } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import JobsSearched from './UserPage/JobsSearched';
 import { useNavigate } from 'react-router-dom';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faHome, faSearch, faBriefcase, faBuilding, faUser } from '@fortawesome/free-solid-svg-icons'
 const NavUser = () => {
-  const [searchkey , setSearchKey] = useState('');
+  const [searchkey, setSearchKey] = useState('');
   const navigate = useNavigate();
   localStorage.setItem('key', searchkey);
   const search = () => {
@@ -17,25 +18,28 @@ const NavUser = () => {
 
   return (
     <div className="pl-24 pt-2 flex justify-between h-12 w-100 bg-white border-b border-gray-400">
-        <div className="flex">
-            <img className="w-8 h-8" src={process.env.PUBLIC_URL + "images/logo.png"} alt="logo"/>
-            <input type="text" className="w-64 h-8 bg-blue-50 rounded px-3 ml-2" value={searchkey} onChange={(e) => setSearchKey(e.target.value)} placeholder="Search" onKeyPress={event => {
-                if (event.key === 'Enter') {
-                  search()
-                }
-              }}/>
+      <div className="flex">
+        <img className="w-8 h-8" src={process.env.PUBLIC_URL + "images/logo.png"} alt="logo" />
+        <input type="text" className="w-64 h-8 bg-blue-50 rounded px-3 ml-2" value={searchkey} onChange={(e) => setSearchKey(e.target.value)} placeholder="Search" onKeyPress={event => {
+          if (event.key === 'Enter') {
+            search()
+          }
+        }} />
+      </div>
+      <div className="flex pr-24">
+        <div className="flex flex-col pl-5">
+          <FontAwesomeIcon icon={faHome} />
+          <Link to="/user">Home</Link>
         </div>
-        <div className="flex pr-24">
-            <div className="flex flex-col pl-5">
-                <FaHome/>
-                <Link to="/user">Home</Link>
-            </div>
-            <div className="flex flex-col pl-5">
-                <FaShoppingBag/>
-                <Link to="/jobs">Jobs</Link>
-            </div>
-            
+        <div className="flex flex-col pl-5">
+          <FontAwesomeIcon icon={faBriefcase} />
+          <Link to="/jobs">Jobs</Link>
         </div>
+        <div className="flex flex-col pl-5">
+          <FontAwesomeIcon icon={faBuilding} />
+          <Link to="/companies">Companies</Link>
+        </div>
+      </div>
     </div>
   )
 }
