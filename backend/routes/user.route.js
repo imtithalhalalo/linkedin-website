3015bb5a6b94ab332c1c1a18c95011aaddb0eefb
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-21 02:02:52 +0300

    backend: add route update image

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const {editProfile, getUser, getJobs, search, apply, followCompany, unFollowCompany} = require('../controllers/user.controller');
+const {editProfile, getUser, getJobs, search, apply, followCompany, unFollowCompany, updateProfileImage} = require('../controllers/user.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const router = Router();
 
@@ -10,4 +10,5 @@ router.get('/search/:key', authMiddleware, search);
 router.post('/apply', authMiddleware, apply);
 router.post('/follow', authMiddleware, followCompany);
 router.post('/unfollow', authMiddleware, unFollowCompany);
+router.post('/updateimage', authMiddleware, updateProfileImage);
 module.exports = router;
