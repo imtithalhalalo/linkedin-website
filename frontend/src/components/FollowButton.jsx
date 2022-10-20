bc82959ece33d66c7418d1bc92d6a42e54fcfbb2
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-20 21:29:45 +0300

    frontend: modify follow button component

--- frontend/src/components/FollowButton.jsx
+++ frontend/src/components/FollowButton.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import axios from 'axios';
 const FollowButton = (props) => {
+  const [onFollow, setOnFollow] = useState(false);
+  const handleClick = async (event) => {
+    event.preventDefault();
+    const data = {
+        follower: localStorage.getItem('id'),
+        following: props.company_id
+    }
+    try {
+        await axios.post(`http://localhost:3001/user/follow`, data,
+            { headers: { 'authorization': `Bearer ${localStorage.getItem(`token`)}` } })
+            .then(response => {
+                setOnFollow(true)
+            });
+    } catch (err) {
+        console.error(err.message);
+    }
+  }
   return (
     <button className="hover:bg-gray-200 text-center rounded-full w-24 bg-white border-2 text-gray-500 py-1 font-medium mt-2" 
-    value={props.value} onClick={props.onClick}>{props.text}</button>
+    company_id={props.value} onClick={ handleClick }>{onFollow ? "Following":"Follow"}</button>
   )
 }
 
