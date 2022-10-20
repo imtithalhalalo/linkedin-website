e16f1b140490cf039ca742ae1c34c0d2e0f167bf
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-20 21:58:27 +0300

    frontend: modify button with unfollow

--- frontend/src/components/FollowButton.jsx
+++ frontend/src/components/FollowButton.jsx
@@ -1,26 +1,39 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
 const FollowButton = (props) => {
   const [onFollow, setOnFollow] = useState(false);
   const handleClick = async (event) => {
     event.preventDefault();
     const data = {
-        follower: localStorage.getItem('id'),
-        following: props.company_id
+      follower: localStorage.getItem('id'),
+      following: props.company_id
     }
-    try {
-        await axios.post(`http://localhost:3001/user/follow`, data,
-            { headers: { 'authorization': `Bearer ${localStorage.getItem(`token`)}` } })
-            .then(response => {
-                setOnFollow(true)
-            });
-    } catch (err) {
+    if (onFollow) {
+      try {
+        await axios.post(`http://localhost:3001/user/unfollow`, data,
+          { headers: { 'authorization': `Bearer ${localStorage.getItem(`token`)}` } })
+          .then(response => {
+            setOnFollow(false)
+          });
+      } catch (err) {
         console.error(err.message);
+      }
+    } else {
+      try {
+        await axios.post(`http://localhost:3001/user/follow`, data,
+          { headers: { 'authorization': `Bearer ${localStorage.getItem(`token`)}` } })
+          .then(response => {
+            setOnFollow(true)
+          });
+      } catch (err) {
+        console.error(err.message);
+      }
     }
+
   }
   return (
-    <button className="hover:bg-gray-200 text-center rounded-full w-24 bg-white border-2 text-gray-500 py-1 font-medium mt-2" 
-    company_id={props.value} onClick={ handleClick }>{onFollow ? "Following":"Follow"}</button>
+    <button className="hover:bg-gray-200 text-center rounded-full w-24 bg-white border-2 text-gray-500 py-1 font-medium mt-2"
+      company_id={props.value} onClick={handleClick}>{onFollow ? "Following" : "Follow"}</button>
   )
 }
 
